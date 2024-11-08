84d959f25c8ee66f3f5ccdf914a49dcd1ffc32dc
JamesIrwin3 <139516801+JamesIrwin3@users.noreply.github.com>
2024-11-08 18:31:13 -0500

    Added express boilerplate

--- index.ts
+++ index.ts
@@ -49,13 +49,52 @@ console.log(chalk.green("Vite environment setup at /vite"), chalk.cyan("Installi
 $ex`${VITE_CWD}npm install`;
 console.log(chalk.green("Installed vite dependencies"), chalk.cyan("Setting up express"));
 fs.mkdirSync(EXPRESS_PATH);
+fs.mkdirSync(path.join(EXPRESS_PATH, "src"));
+fs.mkdirSync(path.join(EXPRESS_PATH, "src/controllers"));
+fs.mkdirSync(path.join(EXPRESS_PATH, "src/routes"));
+fs.mkdirSync(path.join(EXPRESS_PATH, "src/db"));
+
 fs.cpSync(path.join(__dirname, "../express.package.json"), path.join(EXPRESS_PATH, "package.json"));
 $ex`${EXPRESS_CWD}npm install`;
-$ex`${EXPRESS_CWD}touch index.js`;
+$ex`${EXPRESS_CWD}npm i cors`;
+
+fs.writeFileSync(path.join(EXPRESS_PATH, '.env'), `
+MONGO_URI=<YOUR MONGO URI HERE>
+SERVER_PORT=3000
+`)
+
+fs.writeFileSync(path.join(EXPRESS_PATH, 'index.js'), `
+const express = require("express");
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const passport = require('passport');
+
+dotenv.config();
+const { SERVER_PORT, MONGO_URI } = process.env;
+if(MONGO_URI !== "<YOUR MONGO URI HERE>"){
+	mongoose.connect(MONGO_URI);
+	const db = mongoose.connection;
+	db.on('error', (error) =>console.log("[MONGO] Database Error", error?.message)); 
+	db.once('open', () => console.log("[MONGO] Connection successfull"));
+}else{
+	console.warn("No database URI has been provided");
+}
+const app = express();
+app.set('trust proxy', 1);
+app.use(cors({
+	credentials: true,
+	origin: ['http://localhost:3000', 'http://localhost:3001']
+}));
+app.use(express.json());
+app.use(passport.initialize());
+app.listen(SERVER_PORT, ()=>console.log("Listening on port", SERVER_PORT));
+`)
+
 console.log(chalk.green("Express environment setup at /express"), chalk.cyan("Installing dependencies"));
 
 const data = JSON.parse(fs.readFileSync(PROJECT_PACKAGE).toString());
-data.scripts.start = `concurrently "npm start --prefix vite" "npm start --prefix express`;
+data.scripts.start = `concurrently "npm run dev --prefix vite" "npm run start --prefix express"`;
 fs.writeFileSync(PROJECT_PACKAGE, JSON.stringify(data));
 
 
